f741cbb7245fac735582d9177ab3bdf2993b9d01
Daniel Enrico Botta <danielenricobotta@outlook.it>
2020-03-20 01:10:55 +0100

    randomFact better format"

--- bot_handler.py
+++ bot_handler.py
@@ -44,5 +44,6 @@ class Handler():
         import requests
         f = requests.get('https://uselessfacts.jsph.pl/random.txt?language=en')
         text = f.text.split("\n")[0]
+        text = text.replace("> ", "")
         self.log.logInfo(text)
         self.bot.sendMessage(chat_id, text)
--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 import requests
 f = requests.get('https://uselessfacts.jsph.pl/random.txt?language=en')
-
-print(f.text)
-print(f.text.split("\n")[0])
+text = f.text.split("\n")[0]
+text = text.replace("> ", "")
+print(text)
