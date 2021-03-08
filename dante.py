a751664705cdcc1dc3eb3d184166a7e9fd64d2e9
Daniel Enrico Botta <danielenricobotta@outlook.it>
2021-03-09 00:44:48 +0100

    improved the beautiful soup part. Now it adapts better to the dante website

--- dante.py
+++ dante.py
@@ -5,11 +5,9 @@ from bs4 import BeautifulSoup as Soup
 def getDante():
     html = requests.get("https://accademiadellacrusca.it/it/dante")
     soup = Soup(html.content, "html.parser")
-    for br in soup.find_all("br"):
-        br.replace_with("\n" + br.text)
     word = soup.select('div.col-12 h3 i')[0].text
     subword = soup.select('div.col-12 p')[0].text
-    verse = soup.select('div.col-12 blockquote p')[0].text
+    verse = soup.select('div.col-12 blockquote')[0].get_text(separator='\n')
     description = soup.select('div.col-12 p')[2].text
-    return (word, subword, verse, description)
+    return (word.capitalize(), subword, verse, description)
 
---
+++ test.py
@@ -0,0 +1,7 @@
+import dante
+
+(word, sub, ver, desc)=dante.getDante()
+print(word)
+print(sub)
+print(ver)
+print(desc)
